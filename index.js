5fd3848d7ab1620f9f87c4ff7a23f32397d70f0c
DESKTOP-FRK722U\Thilina <dilshanthilina53@gmail.com>
2021-05-30 22:59:10 +0530

    better

---
+++ index.js
@@ -0,0 +1,13 @@
+const arr = ['sudhara', 'ishara', 'maduka', 'liyo']
+
+// array add
+// array ekkata data add karana vidiya
+arr.push('prageesha')
+
+console.log(arr);
+
+// array ekaka thiyendva balanava
+arr.find((val) => val == 'sudhara')
+
+// array ekaka value eke index eka hoya gannava
+const a = arr.indexOf('hdh') // -1
